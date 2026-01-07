140962c3810d8c804a977ff57b5a38bd903f0323
fabiorian <fabiorian718@gmail.com>
2026-01-06 21:41:59 -0300

    Verdadeiro projeto final

--- createpedia/backend/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("✅ Conectado ao MongoDB Atlas"))
-  .catch(err => console.error("❌ Erro MongoDB:", err));
-
-console.log("MONGO_URL =", process.env.MONGO_URL);
-  
